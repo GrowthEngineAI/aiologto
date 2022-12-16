5d57a179925af505ae6cff7e078b5daf64c1aa9a
trisongz <4735784+trisongz@users.noreply.github.com>
2022-12-16 17:25:16 -0600

    publish first version

---
+++ CHANGELOGS.md
@@ -0,0 +1,4 @@
+## aiologto Changelogs
+
+- 0.0.1 (2022-12-16)
+    - Initial release.
---
+++ tests/client.py
@@ -0,0 +1,8 @@
+from aiologto import Logto
+
+Logto.configure(
+    url = '...',
+    app_id = "...",
+    app_secret = "...",
+    debug_enabled = True,
+)
